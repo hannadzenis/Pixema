0b7b28c0a417f55f02f1a854d25875df57cae0ca
Hannah <118190915+hannadzenis@users.noreply.github.com>
2023-10-06 11:59:40 +0200

    fix

--- my-app/src/components/FavMovies.tsx
+++ my-app/src/components/FavMovies.tsx
@@ -1,24 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { OneGenre } from "../Store/getMovies";
 import { useAppSelector } from "../Store/store";
 import { RenderMovie } from "./Movies/RenderMovies";
 
 
 export const FavoriteMovies = () => {
-    const movies = useAppSelector(state => {
+    const favorites = useAppSelector(state => {
         const genresIdsToNames = (genresIds: number[]) => genresIds.map(genreIdToName);
+        console.log(genresIdsToNames)
         const genres: OneGenre[] = state.movies.genres
         const genreIdToName = (genreId: number) => genres.find(({ id }) => id === genreId)?.name
         return state.movies.movies.map(movie => ({ ...movie, genre: genresIdsToNames(movie.genre_ids) }))
     })
 
-    useEffect(()=>{
-        localStorage.getItem('DFX-favourites')
-    }, [])
+    // const [favorites, setFavorites] = useState(() => {
+    //     // getting stored value
+    //     const saved = localStorage.getItem("DFX-favourites");
+    //     const initialValue = JSON.stringify(saved);
+    //     const favs = Object(initialValue)
+    //     return favs;
+    // });
 
-    const favMovies = movies.filter(movie => movie.favorite)
+    const favMovies = favorites.filter(movie => movie.favorite)
     return (
-        <div className="films-wrapper">
+        <div>
             {!favMovies.length && <span>No favorite movies</span>}
             {favMovies.map(movie => <RenderMovie oneMovie={movie} key={movie.id} />)}
         </div>
